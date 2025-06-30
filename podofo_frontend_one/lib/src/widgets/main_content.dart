3e68da50cd0feeb07251c3604f1f5dd972e468ff
Yunho Cho <yhjc2692@gmail.com>
2025-06-29 20:21:59 -0400

    Added simplistic dark mode implementation for pdf viewer

--- podofo_frontend_one/lib/src/widgets/main_content.dart
+++ podofo_frontend_one/lib/src/widgets/main_content.dart
@@ -11,44 +11,61 @@ class MainContent extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final filePath = ref.watch(filePathProvider);
     final pdfViewerController = ref.watch(pdfViewerControllerProvider);
+    final theme = ref.watch(themeProvider);
+    final bool darkMode = theme == ThemeMode.dark;
 
     return Expanded(
       child: Scaffold(
-        appBar: AppBar(
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.folder_open),
-              onPressed: () => ref.read(filePathProvider.notifier).pickFile(),
-            ),
-          ],
-        ),
+        appBar: AppBar(actions: []),
         body: filePath != null
-            ? PdfViewer.file(
-                filePath,
-                controller: pdfViewerController,
-                params: PdfViewerParams(
-                  // enableTextSelection: true, // FOR pdfrx 1.2.9
-                  textSelectionParams: PdfTextSelectionParams(
-                    textSelectionTriggeredBySwipe: true,
-                  ), // FOR pdfrx 1.3.0
-                  linkHandlerParams: PdfLinkHandlerParams(
-                    onLinkTap: (link) {
-                      if (link.url != null) {
-                        // external web URL
-                        launchUrl(link.url!);
-                      } else if (link.dest != null) {
-                        // internal link
-                        pdfViewerController.goToDest(link.dest!);
-                      }
-                    },
-                    linkColor: Color.fromRGBO(100, 100, 255, 0.01),
-                  ),
-                  viewerOverlayBuilder: (context, size, handleLinkTap) => [
-                    PdfViewerScrollThumb(
-                      controller: pdfViewerController,
-                      orientation: ScrollbarOrientation.right,
+            ? ColorFiltered(
+                colorFilter: ColorFilter.mode(
+                  Colors.white,
+                  darkMode ? BlendMode.difference : BlendMode.dst,
+                ),
+                child: PdfViewer.file(
+                  filePath,
+                  controller: pdfViewerController,
+                  params: PdfViewerParams(
+                    // enableTextSelection: true, // FOR pdfrx 1.2.9
+                    textSelectionParams: PdfTextSelectionParams(
+                      textSelectionTriggeredBySwipe: true,
+                    ), // FOR pdfrx 1.3.0
+                    linkHandlerParams: PdfLinkHandlerParams(
+                      onLinkTap: (link) {
+                        if (link.url != null) {
+                          // external web URL
+                          launchUrl(link.url!);
+                        } else if (link.dest != null) {
+                          // internal link
+                          pdfViewerController.goToDest(link.dest!);
+                        }
+                      },
+                      linkColor: const Color.fromRGBO(100, 100, 255, 0.01),
                     ),
-                  ],
+                    viewerOverlayBuilder: (context, size, handleLinkTap) => [
+                      PdfViewerScrollThumb(
+                        controller: pdfViewerController,
+                        orientation: ScrollbarOrientation.right,
+                      ),
+                    ],
+                    backgroundColor: darkMode
+                        ? const Color.fromRGBO(230, 230, 230, 1.0)
+                        : const Color.fromRGBO(250, 250, 250, 1.0),
+                    pageDropShadow: darkMode
+                        ? const BoxShadow(
+                            color: Colors.white54,
+                            blurRadius: 4,
+                            spreadRadius: 0,
+                            offset: Offset(0, 4),
+                          )
+                        : const BoxShadow(
+                            color: Colors.black54,
+                            blurRadius: 4,
+                            spreadRadius: 0,
+                            offset: Offset(0, 4),
+                          ),
+                  ),
                 ),
               )
             : const Center(
