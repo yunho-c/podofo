d15254c1f45448f1f9e1f52f9749e8ac5941c298
Yunho Cho <yhjc2692@gmail.com>
2025-07-07 20:39:05 -0400

    Improved debug pane layout

--- podofo_frontend_one/lib/src/widgets/panes/debug_pane.dart
+++ podofo_frontend_one/lib/src/widgets/panes/debug_pane.dart
@@ -9,67 +9,60 @@ class DebugPane extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final userState = ref.watch(userStateNotifierProvider);
-    final shaderStrength = ref.watch(shaderStrengthProvider);
     final loadedDocuments = ref.watch(loadedDocumentsProvider);
     return Container(
       padding: const EdgeInsets.all(10.0),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Expanded(
-            child: Collapsible(
-              children: [
-                const CollapsibleTrigger(child: Text('Loaded Documents')),
-                CollapsibleContent(
-                  child: OutlinedContainer(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        for (final doc in loadedDocuments.values)
-                          Text(
-                            '${doc.title}: ${doc.lastOpenedPage ?? 'Not set'}',
-                          ).small(),
-                      ],
-                    ).withPadding(horizontal: 16, vertical: 8),
-                  ).withPadding(top: 8),
-                ),
-              ],
-            ),
+          Collapsible(
+            children: [
+              const CollapsibleTrigger(child: Text('Loaded Documents')),
+              CollapsibleContent(
+                child: OutlinedContainer(
+                  child: Column(
+                    children: [
+                      for (final doc in loadedDocuments.values)
+                        Text(
+                          '${doc.title}: ${doc.lastOpenedPage ?? 'Not set'}',
+                        ).small(),
+                    ],
+                  ).withPadding(horizontal: 16, vertical: 8),
+                ).withPadding(top: 8),
+              ),
+            ],
           ),
-          Expanded(
-            child: Collapsible(
-              children: [
-                const CollapsibleTrigger(child: Text('Dark Mode')),
-                CollapsibleContent(
-                  child: OutlinedContainer(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [],
-                    ).withPadding(horizontal: 16, vertical: 8),
-                  ).withPadding(top: 8),
-                ),
-              ],
-            ),
+          const SizedBox(height: 10),
+          Collapsible(
+            children: [
+              const CollapsibleTrigger(child: Text('Dark Mode')),
+              CollapsibleContent(
+                child: OutlinedContainer(
+                  child: Column(
+                    children: [],
+                  ).withPadding(horizontal: 16, vertical: 8),
+                ).withPadding(top: 8),
+              ),
+            ],
           ),
-          Expanded(
-            child: Collapsible(
-              children: [
-                const CollapsibleTrigger(child: Text('User State')),
-                CollapsibleContent(
-                  child: OutlinedContainer(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Highlight').small(),
-                        Text('${userState.highlight}'),
-                        Text('Appearance').small(),
-                        Text('${userState.appearance}'),
-                      ],
-                    ).withPadding(horizontal: 16, vertical: 8),
-                  ).withPadding(top: 8),
-                ),
-              ],
-            ),
+          const SizedBox(height: 10),
+          Collapsible(
+            children: [
+              const CollapsibleTrigger(child: Text('User State')),
+              CollapsibleContent(
+                child: OutlinedContainer(
+                  child: Column(
+                    children: [
+                      Text('Highlight').small(),
+                      Text('${userState.highlight}'),
+                      Text('Appearance').small(),
+                      Text('${userState.appearance}'),
+                    ],
+                  ).withPadding(horizontal: 16, vertical: 8),
+                ).withPadding(top: 8),
+              ),
+            ],
           ),
         ],
       ),
