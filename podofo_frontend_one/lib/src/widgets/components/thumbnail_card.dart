415d41246c8eb24d6da3a5fdf14b0096c53ad072
Yunho Cho <yhjc2692@gmail.com>
2025-07-09 22:32:20 -0400

    WIP: Lifted state mgmt from `ThumbnailCard` to `ThumbnailPane`
    NOTE: This is buggy (goes into infinite loop due to auto-focusing of `ThumbnailCard` when thumbnail pane is scrolled)

--- podofo_frontend_one/lib/src/widgets/components/thumbnail_card.dart
+++ podofo_frontend_one/lib/src/widgets/components/thumbnail_card.dart
@@ -1,64 +1,35 @@
 import 'package:shadcn_flutter/shadcn_flutter.dart';
 
-class ThumbnailCard extends StatefulWidget {
+class ThumbnailCard extends StatelessWidget {
   final Widget thumbnail;
   final String label;
+  final FocusNode focusNode;
+  final ValueChanged<bool> onFocus;
   final VoidCallback onPressed;
 
   const ThumbnailCard({
     super.key,
     required this.thumbnail,
     required this.label,
+    required this.focusNode,
+    required this.onFocus,
     required this.onPressed,
   });
 
   @override
-  State<ThumbnailCard> createState() => _ThumbnailCardState();
-}
-
-class _ThumbnailCardState extends State<ThumbnailCard> {
-  final FocusNode _focusNode = FocusNode();
-  bool _isFocused = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _focusNode.addListener(_handleFocusChange);
-  }
-
-  @override
-  void dispose() {
-    _focusNode.removeListener(_handleFocusChange);
-    _focusNode.dispose();
-    super.dispose();
-  }
-
-  void _handleFocusChange() {
-    if (_focusNode.hasFocus != _isFocused) {
-      setState(() {
-        _isFocused = _focusNode.hasFocus;
-      });
-      if (_focusNode.hasFocus) {
-        widget.onPressed();
-      }
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-
+    // final theme = Theme.of(context);
     return Button(
-      focusNode: _focusNode,
+      focusNode: focusNode,
+      onFocus: onFocus,
       style: ButtonVariance.menu,
       alignment: AlignmentDirectional.centerStart,
-      child: Row(children: [Expanded(child: widget.thumbnail)]),
+      child: Row(children: [Expanded(child: thumbnail)]),
       onPressed: () {
-        if (_focusNode.hasFocus) {
-          widget.onPressed();
-        } else {
-          _focusNode.requestFocus();
+        if (!focusNode.hasFocus) {
+          focusNode.requestFocus();
         }
+        onPressed();
       },
     );
   }
--- podofo_frontend_one/lib/src/widgets/panes/thumbnail_pane.dart
+++ podofo_frontend_one/lib/src/widgets/panes/thumbnail_pane.dart
@@ -1,82 +1,162 @@
 import 'dart:ui';
-import 'package:flutter/material.dart' show Scaffold;
-import 'package:flutter/widgets.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:shadcn_flutter/shadcn_flutter.dart' hide Scaffold;
+import 'package:shadcn_flutter/shadcn_flutter.dart';
 
+import 'package:podofo_one/src/data/document_data.dart';
 import 'package:podofo_one/src/providers/providers.dart';
 import 'package:podofo_one/src/widgets/components/thumbnail_card.dart';
 
-class ThumbnailPane extends ConsumerWidget {
+class ThumbnailPane extends ConsumerStatefulWidget {
   const ThumbnailPane({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final currentDoc = ref.watch(currentDocumentProvider);
+  ConsumerState<ThumbnailPane> createState() => _ThumbnailPaneState();
+}
+
+class _ThumbnailPaneState extends ConsumerState<ThumbnailPane> {
+  List<FocusNode> _focusNodes = [];
+  bool _isNavigating = false;
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    final doc = ref.read(currentDocumentProvider);
+    if (doc != null) {
+      _updateFocusNodes(doc.pdfDocument.pages.length);
+    }
+
+    // Reload FocusNodes if more of PdfDocument is lazily loaded (NOTE: don't remove)
+    // ref.listen<Document?>(currentDocumentProvider, (previous, next) {
+    //   final prevPageCount = previous?.pdfDocument.pages.length;
+    //   final nextPageCount = next?.pdfDocument.pages.length;
+    //   if (mounted && prevPageCount != nextPageCount) {
+    //     setState(() {
+    //       _updateFocusNodes(nextPageCount ?? 0);
+    //     });
+    //   }
+    // });
+  }
+
+  // TODO: handle change of currentDocument (probably rebuild? or reload)
+
+  // @override
+  // void didChangeDependencies() {
+  //   // TODO: implement didChangeDependencies
+  //   super.didChangeDependencies();
+  // }
+
+  void _updateFocusNodes(int count) {
+    _disposeFocusNodes();
+    _focusNodes = List.generate(count, (_) => FocusNode());
+  }
+
+  void _disposeFocusNodes() {
+    for (final node in _focusNodes) {
+      node.dispose();
+    }
+  }
+
+  @override
+  void dispose() {
+    _disposeFocusNodes();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final doc = ref.watch(currentDocumentProvider);
+    // Enable auto-focusing on current page
+    ref.listen(currentDocumentProvider, ((previous, next) {
+      final int currentPage = next?.lastOpenedPage ?? 0;
+      if (mounted &&
+          !_isNavigating &&
+          currentPage != _selectedIndex &&
+          _focusNodes.isNotEmpty &&
+          currentPage < _focusNodes.length &&
+          _focusNodes[currentPage].canRequestFocus) {
+        _focusNodes[currentPage].requestFocus();
+      }
+    }));
+
+    if (doc == null) {
+      return const Center(child: Text('No document loaded'));
+    }
+
+    final currentPage = doc.lastOpenedPage ?? 0;
     final brightness = ref.watch(brightnessProvider);
     final bool darkMode = brightness == Brightness.dark;
     final shader = ref.watch(shaderProvider);
+    final thumbnailsState = ref.watch(thumbnailsProvider);
+    final thumbnails = thumbnailsState.thumbnails[doc.filePath];
+    final isGenerating = thumbnailsState.isGenerating[doc.filePath] ?? false;
     final pdfViewerController = ref.read(pdfViewerControllerProvider);
 
-    if (currentDoc == null) {
-      return const Center(child: Text('No document loaded'));
-    } else {
-      final currentPageNumber = currentDoc.lastOpenedPage ?? 0;
-      final thumbnailsState = ref.watch(thumbnailsProvider);
-      final thumbnails = thumbnailsState.thumbnails[currentDoc.filePath];
-      final isGenerating =
-          thumbnailsState.isGenerating[currentDoc.filePath] ?? false;
+    if (isGenerating && (thumbnails == null || thumbnails.isEmpty)) {
+      return const Center(child: CircularProgressIndicator());
+    }
 
-      if (isGenerating) {
-        return const Center(child: CircularProgressIndicator());
-      }
+    if (thumbnails == null || thumbnails.isEmpty) {
+      return const Center(child: Text('No thumbnails available.'));
+    }
 
-      if (thumbnails == null || thumbnails.isEmpty) {
-        return const Center(child: Text('No thumbnails available.'));
-      }
-
-      return Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: FocusTraversalGroup(
-            child: GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 1,
-                crossAxisSpacing: 8,
-                mainAxisSpacing: 8,
-                childAspectRatio: 1 / 1.38,
-              ),
-              itemCount: thumbnails.length,
-              itemBuilder: (BuildContext context, int index) {
-                final pageNumber = thumbnails.keys.elementAt(index);
-                final thumbnailBytes = thumbnails.values.elementAt(index);
-
-                Widget imageWidget = Image.memory(thumbnailBytes);
-
-                if (darkMode && shader != null) {
-                  imageWidget = ImageFiltered(
-                    imageFilter: ImageFilter.shader(shader),
-                    child: imageWidget,
-                  );
-                }
-                return ThumbnailCard(
-                  thumbnail: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                    child: imageWidget,
-                  ),
-                  label: '${pageNumber + 1}',
-                  onPressed: () {
-                    pdfViewerController.goToPage(pageNumber: index + 1);
-                  },
-                );
-              },
+    return Scaffold(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FocusTraversalGroup(
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 1,
+              crossAxisSpacing: 8,
+              mainAxisSpacing: 8,
+              childAspectRatio: 1 / 1.38,
             ),
+            itemCount: thumbnails.length,
+            itemBuilder: (BuildContext context, int index) {
+              final pageNumber = thumbnails.keys.elementAt(index);
+              final thumbnailBytes = thumbnails.values.elementAt(index);
+
+              Widget imageWidget = Image.memory(thumbnailBytes);
+
+              if (darkMode && shader != null) {
+                imageWidget = ImageFiltered(
+                  imageFilter: ImageFilter.shader(shader),
+                  child: imageWidget,
+                );
+              }
+              return ThumbnailCard(
+                focusNode: _focusNodes[pageNumber],
+                thumbnail: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: imageWidget,
+                ),
+                label: '${pageNumber + 1}',
+                onPressed: () async {
+                  _isNavigating = true;
+                  _selectedIndex = pageNumber;
+                  await pdfViewerController.goToPage(pageNumber: pageNumber);
+                  _isNavigating = false;
+                },
+                onFocus: (hasFocus) async {
+                  if (hasFocus) {
+                    if (pdfViewerController.isReady) {
+                      _isNavigating = true;
+                      _selectedIndex = pageNumber;
+                      await pdfViewerController.goToPage(
+                        pageNumber: pageNumber,
+                      );
+                    }
+                    _isNavigating = false;
+                  }
+                },
+              );
+            },
           ),
         ),
-      );
-    }
+      ),
+    );
   }
 }
